77856fbe7bcbffbea1e48b0de8da115102bc060f
Sherman Marshall <sherman.david.marshall@gmail.com>
2017-12-19 01:16:34 -0800

    Adding code for reading files

---
+++ OpenGL/utils/readFile.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readFile(const char* filename) {
+	size_t MAX_READ = 1024;
+	char* data = (char*) malloc(sizeof(char) * MAX_READ); 
+	char* tmp = NULL;
+	FILE* filePtr = fopen(filename, "r");
+	size_t stringSize = 0;
+
+	//If file was opened
+	if (filePtr) {
+		//Defining input max for fread
+		size_t bytesRead;
+
+		do {	//Read
+			bytesRead = fread(data, 1, MAX_READ, filePtr);
+			if (bytesRead > 0) {
+				printf("%i\n", bytesRead);
+
+				int freeAndSwap = 1;
+				if (bytesRead < MAX_READ) {
+					tmp = (char*) malloc(bytesRead);
+					int num = 0;
+					
+					char* initialData = data;
+					char* initialTmp = tmp;
+					while (num < bytesRead) {
+						*tmp = *data;
+						tmp++;
+						data++;
+						num++;
+					}
+					
+					free(initialData);
+					data = initialTmp;
+					tmp = initialTmp;
+				} else {
+					freeAndSwap = 0;
+				}
+				
+				//Copy a new string
+				char* out = strcpy((char*) malloc(sizeof(char) * (stringSize + bytesRead)), data);
+				stringSize += bytesRead;
+				
+				if (freeAndSwap) {
+					free(data);
+					data = out;
+				}
+				printf("%s\n", data);
+			}
+
+		} while (bytesRead > 0);
+		
+		return data;
+	}
+	return NULL;
+}
+/*
+int main() {
+	//char* str = readFile("oneGbOfCaps.txt");
+	//printf("%s\n", str);
+}
+*/
